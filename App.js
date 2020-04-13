53dd2acfed4f3bfb2f760c7b945b4cda5522a16e
Essam <essamabuissa@hotmail.com>
2020-04-13 13:42:07 +0300

    Adding RootNavigator

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
+//Navigation
+import { NavigationContainer } from "@react-navigation/native";
+
+//Redux
+import RootNavigator from "./Navigation/index";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
