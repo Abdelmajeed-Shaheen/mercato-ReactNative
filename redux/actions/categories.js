dcf275d005bb874b50e9e3f77be7fb23701ae571
Essam <essamabuissa@hotmail.com>
2020-04-15 00:07:26 +0300

    Category action added

---
+++ redux/actions/categories.js
@@ -0,0 +1,15 @@
+import { SET_CATEGORIES } from "./actionTypes";
+import instance from "./instance";
+
+export const getCategories = () => async (dispatch) => {
+  try {
+    const response = await instance.get("api/categories");
+    const categories = response.data;
+    dispatch({
+      type: SET_CATEGORIES,
+      payload: categories,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
