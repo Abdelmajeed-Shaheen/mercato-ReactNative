2008330992a0eccf924806a6acaeeccf7d5a923c
Essam <essamabuissa@hotmail.com>
2020-04-15 00:02:07 +0300

    Added Category Stack

--- Navigation/index.js
+++ Navigation/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { USER } from "./screenNames";
+import { USER, CATEGORY } from "./screenNames";
 
 //UserStack
 import UserStack from "./StackNavigators/UserStack";
+import Category from "../Components/CategoryList/CategoryItem";
+import CategoryStack from "./StackNavigators/CategoryStack";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -11,6 +13,7 @@ function RootNavigator() {
   return (
     <Navigator>
       <Screen name={USER} component={UserStack} />
+      <Screen name={CATEGORY} component={CategoryStack} />
     </Navigator>
   );
 }
