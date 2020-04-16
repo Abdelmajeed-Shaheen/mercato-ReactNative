637c2d98df0512c2df03521fbf6b4730cb225648
Essam <essamabuissa@hotmail.com>
2020-04-16 23:50:08 +0300

    Added Item Detail Screen

--- Navigation/StackNavigators/CategoryStack.js
+++ Navigation/StackNavigators/CategoryStack.js
@@ -3,9 +3,10 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import screenOptions from "../screenOptions";
-import { CATEGORY, ITEMS } from "../screenNames";
+import { CATEGORY, ITEMS, ITEM } from "../screenNames";
 import CategoryList from "../../Components/CategoryList/index";
-import ItemsList from "../../Components/ItemsList.js/index";
+import ItemsList from "../../Components/ItemsList/index";
+import ItemDetail from "../../Components/ItemDetail/index";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -19,6 +20,11 @@ const CategoryStack = () => (
     <Screen
       name={ITEMS}
       component={ItemsList}
+      options={{ headerShown: false }}
+    />
+    <Screen
+      name={ITEM}
+      component={ItemDetail}
       options={{ headerShown: false }}
     />
   </Navigator>
