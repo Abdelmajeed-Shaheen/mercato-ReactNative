7768bfbac0a8561ef8817b7f2634c953a201593b
Essam <essamabuissa@hotmail.com>
2020-04-16 23:45:04 +0300

    Added styles

---
+++ Components/ItemDetail/styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  text: {
+    color: "black",
+    fontSize: 15,
+    marginLeft: 16,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  divider: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  top: {
+    marginLeft: 0,
+    backgroundColor: "#ffffcc",
+  },
+  middleText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 17,
+    alignSelf: "center",
+  },
+  addIcon: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "white",
+  },
+  item: {
+    color: "white",
+  },
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  yellow: {
+    color: "yellow",
+  },
+});
+
+export default styles;
