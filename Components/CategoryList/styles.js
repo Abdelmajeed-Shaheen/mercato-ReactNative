1506cf24679328166a69e111fca3df904c72689c
Essam <essamabuissa@hotmail.com>
2020-04-16 23:43:54 +0300

    Updating styles

--- Components/CategoryList/styles.js
+++ Components/CategoryList/styles.js
@@ -46,6 +46,10 @@ const styles = StyleSheet.create({
   background: {
     width: null,
     flex: 1,
+    backgroundColor: "rgb(70,144,69)",
+  },
+  yellow: {
+    color: "yellow",
   },
 });
 export default styles;
