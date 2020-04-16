dd8a215e6c5e0adb71548168e08f29ff2620f35e
Essam <essamabuissa@hotmail.com>
2020-04-16 23:47:05 +0300

    Added styles

---
+++ Components/ItemsList/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  background: {
+    width: null,
+    flex: 1,
+    backgroundColor: "rgb(70,144,69)",
+  },
+  yellow: {
+    color: "yellow",
+  },
+});
+export default styles;
